6b33136199fa6eb0213062fbc213a6b027d5a8d2
Tarek <tarekreda101@gmail.com>
2023-11-02 07:23:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 def get_client():
-    client = MongoClient(host='172.30.250.124',
-                         port=27017, 
-                         username='adminuser', 
-                         password='adminpass1234',
-                        authSource="records_db")
+    client = MongoClient('localhost',port=27017,username='adminuser', password='adminpass1234', authSource="records_db")
     return client
 
 
